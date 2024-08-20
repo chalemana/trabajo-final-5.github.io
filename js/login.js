029a02eaa952b7986a00959c54f8ad46ac09a2db
PilarA228 <pilaracuna8@gmail.com>
2024-08-20 12:44:43 -0300

    Update login.js

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', funcion(e){
+   
+    document.getElementById('submitBtn').addEventListener('click',()=> {
+        let usuario =document.getElementById('inputEmail').value;
+        let contraseña=document.getElementById('inputPassword').value;
+    
+        if (usuario===null || contraseña===null){
+            alert ("Debe iniciar sesión para continuar");
+        }else{
+            localStorage.setItem("user",usuario);
+            location.href="index.html";
+             } 
+    })
+    });
