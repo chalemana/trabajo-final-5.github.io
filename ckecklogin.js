64a2396ed9ffb0d8a6b1fd3e9b006d6213b8a302
chalemana <chalemanaca@gmail.com>
2024-08-22 14:42:12 -0300

    Delete ckecklogin.js

--- ckecklogin.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-let usuario=localStorage.getItem("user")
-
-if (usuario===null) {
-location.href="login.html";
-}else{
-    document.getElementById('userEmail').innerText=usuario;
-}  
-});
