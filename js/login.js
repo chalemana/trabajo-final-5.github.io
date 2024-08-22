8ab614cbe78c7dc416c383843d1db6568c38dbe1
chalemana <chalemanaca@gmail.com>
2024-08-22 14:39:00 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,13 +12,3 @@ document.addEventListener('DOMContentLoaded', funcion(e){
              } 
     })
     });
-
-document.addEventListener('DOMContentLoaded', ()=> {
-let usuario=localStorage.getItem("user")
-
-if (usuario===null) {
-location.href="login.html";
-}else{
-    document.getElementById('userEmail').innerText=usuario;
-}  
-});
