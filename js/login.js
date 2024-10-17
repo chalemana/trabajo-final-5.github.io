45d09bb34fa3511825f2e4b611610422fd25f8cd
Priosorio <priosoriodeleon@hotmail.com>
2024-10-17 14:42:25 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let user = localStorage.getItem('user');
+    if (user) {
+        // Si ya hay usuario logueado, redirigir al index
+        window.location = 'index.html';
+    } else {
+        const form = document.getElementById('loginForm');
+        if (form) {
+            form.addEventListener('submit', (event) => {
+                event.preventDefault();
+                let usuario = document.getElementById('inputEmail').value;
+                let contraseña = document.getElementById('inputPassword').value;
 
-    document.getElementById('submit').addEventListener('click', (event) => {
-        event.preventDefault();
-        let usuario = document.getElementById('inputEmail').value;
-        let contraseña = document.getElementById('inputPassword').value;
-
-        if (usuario === "" || contraseña === "") {
-            alert("Debe iniciar sesión para continuar");
+                if (usuario === "" || contraseña === "") {
+                    alert("Debe ingresar usuario y contraseña");
+                } else {
+                    // Guardamos el usuario en localStorage para simular una sesión
+                    localStorage.setItem("user", usuario);
+                    // Redirigimos al index.html
+                    window.location.href = "index.html";
+                }
+            });
         } else {
-            localStorage.setItem("user", usuario); 
-            location.href = "index.html";
-        }
-    });
-});
+            document.getElementById('submit').addEventListener('click', (event) => {
+                event.preventDefault();
+                let usuario = document.getElementById('inputEmail').value;
+                let contraseña = document.getElementById('inputPassword').value;
 
+                if (usuario === "" || contraseña === "") {
+                    alert("Debe ingresar usuario y contraseña");
+                } else {
+                    localStorage.setItem("user", usuario);
+                    window.location.href = "index.html";
+                }
+            });
+        }
+    }
+});
