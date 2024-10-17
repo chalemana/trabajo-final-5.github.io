8c99c7ee70e9660c0af4caefc7da1e79f90ca651
Martina Schröder <chalemanaca@gmail.com>
2024-10-17 17:52:44 -0300

    cambios myperfile

--- js/login.js
+++ js/login.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let user = localStorage.getItem('user');
+    const user = localStorage.getItem('user');
+    const currentPage = window.location.pathname.split('/').pop();
+
     if (user) {
-        // Si ya hay usuario logueado, redirigir al index
-        window.location = 'index.html';
+        // Si hay usuario logueado, redirigir solo si no es product-info.html
+        if (currentPage === 'login.html') {
+            window.location.href = 'index.html';
+        }
     } else {
         const form = document.getElementById('loginForm');
         if (form) {
@@ -14,23 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (usuario === "" || contraseña === "") {
                     alert("Debe ingresar usuario y contraseña");
                 } else {
-                    // Guardamos el usuario en localStorage para simular una sesión
                     localStorage.setItem("user", usuario);
-                    // Redirigimos al index.html
-                    window.location.href = "index.html";
-                }
-            });
-        } else {
-            document.getElementById('submit').addEventListener('click', (event) => {
-                event.preventDefault();
-                let usuario = document.getElementById('inputEmail').value;
-                let contraseña = document.getElementById('inputPassword').value;
-
-                if (usuario === "" || contraseña === "") {
-                    alert("Debe ingresar usuario y contraseña");
-                } else {
-                    localStorage.setItem("user", usuario);
-                    window.location.href = "index.html";
+                    window.location.href = "index.html"; // Redirigir al index.html
                 }
             });
         }
